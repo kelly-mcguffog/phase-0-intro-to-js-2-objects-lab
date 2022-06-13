64022bbd575afb1cc6397bc39a4076ebb57efa1e
Kelly McGuffog <kellymcguffog@gmail.com>
2022-06-13 08:20:22 -0400

    commit tree ecdf57ff7cbd74ee105f8c368682accfef3674a6

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+ name: "Kelly",
+ streetAddress: "First Avenue"   
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return{ 
+        ...employee,
+       [key]: value
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key, value){
+    const newObj = {...employee};
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value){
+    delete employee[key];
+    return employee;
+}
